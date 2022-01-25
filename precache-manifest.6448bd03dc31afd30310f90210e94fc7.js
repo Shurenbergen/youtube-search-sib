9a1f4ff9c5486210301102ca459c689ade0a1f37
Shurenbergen <sham-aleksandr@yandex.ru>
2022-01-25 21:10:31 +0000

    Updates

---
+++ precache-manifest.6448bd03dc31afd30310f90210e94fc7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "602dbb618ff3f46f7cb8db2eeb7b73fb",
+    "url": "/youtube-search-sib/index.html"
+  },
+  {
+    "revision": "e7061d5e0b79c1e6b9c6",
+    "url": "/youtube-search-sib/static/css/2.45643e88.chunk.css"
+  },
+  {
+    "revision": "3ed6fe0dd5e2f1039729",
+    "url": "/youtube-search-sib/static/css/main.79fcfbd1.chunk.css"
+  },
+  {
+    "revision": "e7061d5e0b79c1e6b9c6",
+    "url": "/youtube-search-sib/static/js/2.1ffa360a.chunk.js"
+  },
+  {
+    "revision": "6c99299aa0ccf7cd363681b2638ef8ef",
+    "url": "/youtube-search-sib/static/js/2.1ffa360a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3ed6fe0dd5e2f1039729",
+    "url": "/youtube-search-sib/static/js/main.f56152a9.chunk.js"
+  },
+  {
+    "revision": "9c24109cfa938e8b9fdc",
+    "url": "/youtube-search-sib/static/js/runtime-main.ff0307ac.js"
+  }
+]);
